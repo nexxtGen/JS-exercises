c8a8cd1a2ab661acbdb2425524e81d273e210a42
Kamil Szczypior <kszczypior1@gmail.com>
2018-08-21 12:45:54 +0200

    Add Mustache plugin 1.0

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+//Mustache
+
+
+
+
+var templateCell = document.getElementById('template-carousel-cell').innerHTML;
+var results = document.getElementById('results');
+Mustache.parse(templateCell);
+
+var listCells = '';
+
+for(var i = 0; i < dataCell.length; i++){
+  console.log(dataCell);  
+  listCells += Mustache.render(templateCell, dataCell[i]);
+}
+results.insertAdjacentHTML('beforeend', listCells);
+
+
+
+// Flickity
+var restartButton = document.getElementById('first-cell');
+var elem = document.querySelector('.main-carousel');
+var progressBar = document.querySelector('.progress-bar')
+// Add flickity
+var flkty = new Flickity( elem, {   
+  cellAlign: 'left',
+  contain: true,
+  hash: true,
+  pageDots: false,  
+});
+// progress bar
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+// restart button
+restartButton.addEventListener("click", function(){
+    flkty.select(0);
+});
+
+// element argument can be a selector string
+//   for an individual element
