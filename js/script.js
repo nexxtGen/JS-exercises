aa3d26b4a5bdfac8fe0a4602f19bff7b9ca42fd7
Kamil Szczypior <kszczypior1@gmail.com>
2018-08-28 08:05:25 +0200

    Change script to fetch method

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ button.addEventListener('click', function(){
     getJoke();
 });
 
+//Use standard XMLHttp request
+/*
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -17,3 +19,37 @@ function getJoke() {
     xhr.send();
 }
 getJoke();
+*/
+//Use Fetch 
+function getJoke(){
+    fetch(url, { cache: "no-store" })
+        .then(res => res.json())
+        .then(res => {
+            outputJoke.innerHTML = (res.value.joke);
+        })
+}
+getJoke();
+
+
+/*
+var data = { joke: ''};
+function getJoke() {
+    fetch(url, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        //.then(createJoke);
+        .then(function(myJson) {
+            //console.log(JSON.stringify(myJson));
+            var data = myJson[0];
+            console.log(data.value.joke);
+        });
+}
+*/
+/*
+function createJoke(input) {
+    var data = input[0];
+    outputJoke.innerHTML = data.content;
+}
+getJoke();
+*/
