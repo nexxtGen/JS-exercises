466e3a62ca6d75a3c608001961e66aed41b8be86
Kamil Szczypior <kszczypior1@gmail.com>
2018-09-19 01:08:02 +0200

    Add Node script

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 
-var OSinfo = require('./modules/OSinfo'); //Do zmiennej przypisuję plik z moja wyeksportowaną funkcją.
-var EventEmitter = require('events').EventEmitter;  //Wyciągam klasę event emitter bo tylko ona jest mi teraz potrzebna.
 
-var emitter = new EventEmitter();  //Tworzę obiekt na podstawie klasy 'EventEmitter();'
+var fs = require('fs');// fs to moduł Node do pracy z plikami.
+var StatMode = require('stat-mode');
+var colors = require('colors');
 
-// Dodaję nasłuchiwania na zdarzenia.
-emitter.on('beforeCommand', function(instruction) {
-    console.log('You wrote: ' + instruction + ' trying to run command.')
+/*
+fs.stat('./wolf.jpg', function(err, stats) { //fs.stat pozwala na spr czy ścieżka do kt się odnosi istnieje.
+    var statMode = new StatMode(stats);
+    console.log(stats);
 });
-emitter.on('afterCommand', function() {
-console.log('Finished command');
-});
-
-
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-         // odpalanie zdarzenia beforeCommand (z parametrem)
-        emitter.emit('beforeCommand', instruction);
-        switch (instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/ver':                        
-                process.stdout.write('Node version: ' + process.versions.node + '\n' );                               
-                break; 
-            case '/getOSinfo':                        
-                OSinfo.print(); //Odnoszę się do zmiennej z przyp. plikiem i mam dostęp do funkcji getOSinfo, która w tamtym pliku została przypisana właśnie do print i wyeksportowana.                        
-                break;            
-            default:
-                process.stderr.write('Wrong instruction!');
-                break;
-        };
-        // emitowanie zdarzenia afterCommand (bez parametru)
-        emitter.emit('afterCommand');        
-    }
+*/
+/*
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log(data);
 });
 
+fs.writeFile('./tekst.txt', 'Tekst, który zapiszemy w pliku tekst.txt', function(err) {
+    if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
+    console.log('Zapisano!');
+});
+*/
+
+/*
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nZapis drugi', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
+});
+*/
+var path1 = './'
+fs.readdir(path1, function(err, files){ 
+    if (err) throw err;   
+    console.log('Pliki:', files);
+    fs.writeFile('./YourFiles.txt', files, function(err) {
+        if (err) throw err;
+        console.log('Zapisano dane!'.red);
+        fs.readFile('./YourFiles.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie:'.red)
+            console.log(data);
+        });
+    })
+});
